2e9cdb42148e4eb8fa9110b564a515c655825b19
kingaurelio1 <aurelior997@ciencias.unam.mx>
2025-03-05 21:22:16 -0600

    Se resuelve el problema 1768 de Leetcode

---
+++ __pycache__/leetcode1768.py
@@ -0,0 +1,21 @@
+#You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+def MergeStr(s1,s2):
+    pointer1=0
+    s=""
+    if len(s1)< len(s2):
+        while pointer1<len(s1):
+            s+=s1[pointer1]+s2[pointer1]
+            pointer1+=1
+        s+=s2[pointer1::]
+    elif len(s1) > len(s2):
+        while pointer1<len(s2):
+            s+=s1[pointer1]+s2[pointer1]
+            pointer1+=1
+        s+=s2[pointer1::]
+    else:
+        while pointer1<len(s1):
+            s+=s1[pointer1]+s2[pointer1]
+            pointer1+=1
+    return s
+print(MergeStr("ab","pqrs"))
