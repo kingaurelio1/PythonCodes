83c9601eeab58782bf82d4b6e22c15104788a526
kingaurelio1 <aurelior997@ciencias.unam.mx>
2025-03-06 16:17:39 -0600

    Se resolvio el problema 189 de leetcode

---
+++ __pycache__/leetcode189.py
@@ -0,0 +1,21 @@
+#Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+def Rotate(a,k):
+    k=k%len(a)
+    l,r=0,len(a)-1
+    while l < r:
+        a[l],a[r]=a[r],a[l]
+        l+=1
+        r-=1
+    l,r=0,k-1
+    while l<r:
+        a[l],a[r]=a[r],a[l]
+        l+=1
+        r-=1
+    l,r=k,len(a)-1
+    while l < r:
+        a[l],a[r]=a[r],a[l]
+        l+=1
+        r-=1
+    return a
+print(Rotate([1,2,3,4,5,6,7,8,9,10],12))
